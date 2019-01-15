71aa08ac54dc482ceaf775da4aa969153636724e
lb42 <lou.burnard@retired.ox.ac.uk>
2019-01-15 21:34:33 +0000

    add authorlister

--- refresh.sh
+++ refresh.sh
@@ -1,14 +1,14 @@
-cd ELTeC-cze; git pull; cd ..
-cd ELTeC-eng; git pull; cd ..
-cd ELTeC-fra; git pull; cd ..
-cd ELTeC-hun; git pull; cd ..
-cd ELTeC-ita; git pull; cd ..
-cd ELTeC-nor; git pull; cd ..
-cd ELTeC-por; git pull; cd ..
-cd ELTeC-rom; git pull; cd ..
-cd ELTeC-slv; git pull; cd ..
-cd ELTeC-spa; git pull; cd ..
-cd ELTeC-srp; git pull; cd ..
+cd ELTeC-cze; pwd; git pull; cd ..
+cd ELTeC-eng; pwd; git pull; cd ..
+cd ELTeC-fra; pwd; git pull; cd ..
+cd ELTeC-hun; pwd; git pull; cd ..
+cd ELTeC-ita; pwd; git pull; cd ..
+cd ELTeC-nor; pwd; git pull; cd ..
+cd ELTeC-por; pwd; git pull; cd ..
+cd ELTeC-rom; pwd; git pull; cd ..
+cd ELTeC-slv; pwd; git pull; cd ..
+cd ELTeC-spa; pwd; git pull; cd ..
+cd ELTeC-srp; pwd; git pull; cd ..
 
 #saxon -xi corpus=SLV  driver.tei ../Scripts/reporter1.xsl > slv.html
 
