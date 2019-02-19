a58d421b0d668e51b2ed2c065dec6e24354b1c1d
lb42 <lou.burnard@retired.ox.ac.uk>
2019-02-19 13:57:17 +0000

    attempting to make python scripts

---
+++ report.py
@@ -0,0 +1,18 @@
+import glob
+import subprocess
+import os
+
+repoRoot='/home/lou/Public/ELTeC-'
+webRoot='/home/lou/Public/WG1/distantreading.github.io/ELTeC/'
+reporter='/home/lou/Public/Scripts/reporter.xsl'
+
+LANGS=('cze', 'deu', 'eng', 'fra', 'hun', 'ita', 'nor', 'por', 'rom', 'slv', 'spa', 'srp')
+
+for LANG in LANGS:
+    repoName=repoRoot+LANG
+    print("Reporting on repo "+repoName)
+    command="saxon -xi -s:" + repoName + "/driver.tei -xsl:" + reporter + ' corpus='+LANG + ' >'+webRoot+LANG+'/indox.html'
+#    print(command)
+    subprocess.check_output(command,shell=True)
+    
+
