9f8814524999e36adc9a8517c6b7d38eea4504c1
lou burnard <lou.burnard@retired.ox.ac.uk>
2020-04-15 16:20:49 +0100

    update expose.py

--- expose.py
+++ expose.py
@@ -4,9 +4,35 @@ import os
 
 repoRoot='/home/lou/Public/ELTeC-'
 webRoot='/home/lou/Public/distantreading.github.io/ELTeC/'
-reporter='/home/lou/Public/Scripts/expose.xsl'
-
 LANGS=('cze', 'deu', 'eng', 'fra', 'gre', 'hun', 'ita', 'lit', 'nor', 'por', 'rom', 'slv', 'spa', 'srp')
+folderRoot='https://raw.githubusercontent.com/COST-ELTeC/ELTeC-'
+
+
+string1='''<!DOCTYPE html>
+ <html><head>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">     
+  <link rel="stylesheet" href="../../css/cetei.css" media="all" title="no title" charset="utf-8"/>
+  <link type="application/tei+xml" rel="alternative" href="'''
+
+string2='''"/>
+<script src="../../js/CETEI.js" charset="utf-8"></script>
+ <script type="text/javascript">
+    var ceteicean = new CETEI();
+    ceteicean.shadowCSS = "../../css/cetei.css";
+    ceteicean.getHTML5("'''
+
+string3='''",function(data) {
+  document.getElementsByTagName("body")[0].appendChild(data);});
+  </script>
+   <title><xsl:value-of select="Generated dummy"/></title>
+    </head>
+    <body>
+        <a href="https://www.distant-reading.net/">
+            <img src="../../media/distantreading.png" alt="logo"/>
+        </a>
+    </body>
+</html>'''
+
 
 for LANG in LANGS:
     repoName=repoRoot+LANG
@@ -17,11 +43,15 @@ for LANG in LANGS:
         bf=os.path.splitext(FILE)[0] 
         f1=bf.split('/')[1]
         id=f1.split('_')[0]     
-        print("File="+FILE+" id="+id)
-        command="saxon -s:" + repoName + "/" + FILE + \
-         " -xsl:" + reporter + ' lang='+LANG + ' fileName=' + FILE + \
-          ' >'+webRoot+LANG+'/'+id+'.html'
-        #print(command)
-        subprocess.check_output(command,shell=True)
-    
+        outputFile=webRoot+LANG+"/"+id+".html"
+        gitURL=folderRoot+LANG+"/master/"+FILE
+        webFile=open(outputFile,'w')
+        webFile.write(string1+gitURL+string2+gitURL+string3)
+        webFile.close
+ 
 
+
+
+
+ 
+ 
